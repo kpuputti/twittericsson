ef2ae88d9f66eff243960ef781652580ec98ce31
Hannu Lyytikainen <lyyder@gmail.com>
2010-02-15 20:29:10 +0200

    Location aware functionalities improved.

--- static/js/actions.js
+++ static/js/actions.js
@@ -1,29 +1,70 @@
 var LATITUDE = '';
-var LOGNITUDE = '';
+var LONGITUDE = '';
 
-
+/**
+ * Updates the tweet feed.
+ */
 function updateFeed() {
+    console.log('juuh');
+    var geocoder = new google.maps.Geocoder();
 
     $.ajax({
 	    type: 'GET',
 	    url: 'http://api.twitter.com/1/statuses/home_timeline.json?callback=?',
 	    dataType: 'json',
 		success: function(data) {
-		$.each(data, function(i, res){		 
-		        var tweet = '<div class="tweet">' +
-			    '<p><b>' + res.user.screen_name + '</b> ' +
-			    res.text + ' <span class="source">from ' + 
-			    res.source + '</span></p>' +
-			    '</div>';
-				  
-		        $('.feed').append(tweet).show();
-			console.log('text: ' + res.text);
-			console.log('geo: ' + res.geo);
+		$.each(data, function(i, res){		
+			
+			var location = '';
+			if (res.geo != null) {
+			    // location data in the tweet
+    
+			    var latlng = new google.maps.LatLng(res.geo.coordinates[0], res.geo.coordinates[1]);
+			    geocoder.geocode({'latLng': latlng}, function(results, status) {
+				    if (status == google.maps.GeocoderStatus.OK) {
+					if (results[2]) {
+
+					    location = results[2].formatted_address; 
+					    var tweet = '<div class="tweet">' +
+						'<p><b>' + res.user.screen_name + '</b> ' +
+						res.text + ' <span class="source">from ' +
+						res.source + ' at ' + location + '</span>' +
+						'</p>' +'</div>';
+				
+					    $('.feed').append(tweet);
+					    
+					} else {
+					    alert("No results found");
+					}
+				    } else {
+					alert("Geocoder failed due to: " + status);
+				    }
+				});
+
+			}
+			else {
+			    // no location data in the tweet, just print the text
+			        var tweet = '<div class="tweet">' +
+				    '<p><b>' + res.user.screen_name + '</b> ' +
+				    res.text + ' <span class="source">from ' +
+				    res.source + location + '</span>' +
+				    '</p>' +'</div>';
+
+				$('.feed').append(tweet);
+
+			}
 	        });
 	     }
 	});
+    //show the update feed
+    $('.feed').show();
 }
 
+/**
+ *
+ *Sends a new tweet.
+ *
+ */
 function tweet() {
 
     var post = $('#tweet_text').val();
@@ -51,6 +92,9 @@ function tweet() {
    
 }
 
+/**
+ * Resolves the current location of the user.
+ */
 function getGeoData() {
 
     //determine if the handset has client side geo location capabilities
