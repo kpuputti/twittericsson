273ce221a8493e99547d98d4b12effe0d8e81dea
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-03-18 17:23:54 +0200

    Added basic structure for js file.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,2 +1,20 @@
 /*jslint white: true, browser: true, devel: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
 /*global google:false, $:false, jQuery:false*/
+
+// Logger for Firebug/Webkit console.
+var LOG = function (s) {
+    try {
+        console.log(s);
+    } catch (e) {}
+};
+
+// Global namespace for all code.
+var TE = {};
+
+TE.init = function () {
+    LOG('init app');
+};
+
+$(document).ready(function () {
+    TE.init();
+});
