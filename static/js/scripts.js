90c5a064a5b47643ff6105fc53fac7f1b0a281e3
Kimmo Puputti <kimmo.puputti@hexagonit.fi>
2010-03-18 15:53:45 +0200

    Added jslint options to the js file.

---
+++ static/js/scripts.js
@@ -0,0 +1,2 @@
+/*jslint white: true, browser: true, devel: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
+/*global google:false, $:false, jQuery:false*/
